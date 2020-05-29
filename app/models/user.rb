080dd669ffa5cf47edea0637722e371e6a89b19c
takeshi075 <real.feet15@gmail.com>
2020-05-29 20:29:13 +0900

    ヘッダー完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_one :profile, dependent: :destroy
+  mount_uploader :image, ImageUploader
 
   validates :username,presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
