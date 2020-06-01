0a51e62df26701e6d677a2eec540bd3789335809
takeshi075 <real.feet15@gmail.com>
2020-06-01 22:35:30 +0900

    コントローラー、ヘッダーパス変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,11 @@ class PostsController < ApplicationController
 
   def create
     Post.create(post_params)
+    redirect_to root_path
   end
 
   private
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:memo, :menu, :post_image)
   end
 end
