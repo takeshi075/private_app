762c184ca348e0dff56e115cb83cf8a13b153a13
takeshi075 <real.feet15@gmail.com>
2020-06-08 21:17:37 +0900

    アクティブハッシュ 　項目追加

--- app/models/repetition.rb
+++ app/models/repetition.rb
@@ -1,21 +1,21 @@
 class Repetition < ActiveHash::Base
   self.data = [
-      {id: 1, Repetition: '1'}, {id: 2, Repetition: '2'}, {id: 3, Repetition: '3'},
-      {id: 4, Repetition: '4'}, {id: 5, Repetition: '5'}, {id: 6, Repetition: '6'},
-      {id: 7, Repetition: '7'}, {id: 8, Repetition: '8'}, {id: 9, Repetition: '9'},
-      {id: 10, Repetition: '10'}, {id: 11, Repetition: '11'}, {id: 12, Repetition: '12'},
-      {id: 13, Repetition: '13'}, {id: 14, Repetition: '14'}, {id: 15, Repetition: '15'},
-      {id: 16, Repetition: '16'}, {id: 17, Repetition: '17'}, {id: 18, Repetition: '18'},
-      {id: 19, Repetition: '19'}, {id: 20, Repetition: '20'}, {id: 21, Repetition: '21'},
-      {id: 22, Repetition: '22'}, {id: 23, Repetition: '23'}, {id: 24, Repetition: '24'},
-      {id: 25, Repetition: '25'}, {id: 26, Repetition: '26'}, {id: 27, Repetition: '27'},
-      {id: 28, Repetition: '28'}, {id: 29, Repetition: '29'}, {id: 30, Repetition: '30'},
-      {id: 31, Repetition: '31'}, {id: 32, Repetition: '32'}, {id: 33, Repetition: '33'},
-      {id: 34, Repetition: '34'}, {id: 35, Repetition: '35'}, {id: 36, Repetition: '36'},
-      {id: 37, Repetition: '37'}, {id: 38, Repetition: '38'}, {id: 39, Repetition: '39'},
-      {id: 40, Repetition: '40'}, {id: 41, Repetition: '41'}, {id: 42, Repetition: '42'},
-      {id: 43, Repetition: '43'}, {id: 44, Repetition: '44'}, {id: 45, Repetition: '45'},
-      {id: 46, Repetition: '46'}, {id: 47, Repetition: '47'}, {id: 48, Repetition: '48'},
-      {id: 49, Repetition: '49'}, {id: 50, Repetition: '50'}
+      {id: 0, Repetition: 'なし'},{id: 1, Repetition: '1回'}, {id: 2, Repetition: '2回'}, 
+      {id: 3, Repetition: '3回'},{id: 4, Repetition: '4回'}, {id: 5, Repetition: '5回'}, 
+      {id: 6, Repetition: '6回'},{id: 7, Repetition: '7回'}, {id: 8, Repetition: '8回'}, 
+      {id: 9, Repetition: '9回'},{id: 10, Repetition: '10回'}, {id: 11, Repetition: '11回'}, 
+      {id: 12, Repetition: '12回'},{id: 13, Repetition: '13回'}, {id: 14, Repetition: '14回'}, 
+      {id: 15, Repetition: '15回'},{id: 16, Repetition: '16回'}, {id: 17, Repetition: '17回'}, 
+      {id: 18, Repetition: '18回'},{id: 19, Repetition: '19回'}, {id: 20, Repetition: '20回'}, 
+      {id: 21, Repetition: '21回'},{id: 22, Repetition: '22回'}, {id: 23, Repetition: '23回'}, 
+      {id: 24, Repetition: '24回'},{id: 25, Repetition: '25回'}, {id: 26, Repetition: '26回'}, 
+      {id: 27, Repetition: '27回'},{id: 28, Repetition: '28回'}, {id: 29, Repetition: '29回'}, 
+      {id: 30, Repetition: '30回'},{id: 31, Repetition: '31回'}, {id: 32, Repetition: '32回'}, 
+      {id: 33, Repetition: '33回'},{id: 34, Repetition: '34回'}, {id: 35, Repetition: '35回'}, 
+      {id: 36, Repetition: '36回'},{id: 37, Repetition: '37回'}, {id: 38, Repetition: '38回'}, 
+      {id: 39, Repetition: '39回'},{id: 40, Repetition: '40回'}, {id: 41, Repetition: '41回'}, 
+      {id: 42, Repetition: '42回'},{id: 43, Repetition: '43回'}, {id: 44, Repetition: '44回'},
+      {id: 45, Repetition: '45回'},{id: 46, Repetition: '46回'}, {id: 47, Repetition: '47回'}, 
+      {id: 48, Repetition: '48回'},{id: 49, Repetition: '49回'}, {id: 50, Repetition: '50回'} 
   ]
 end
