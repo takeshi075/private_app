48900df049e10c1841a9ba7f710214bbdaaf9368
takeshi075 <real.feet15@gmail.com>
2020-06-19 09:40:41 +0900

    プロフィール画像、登録ある場合とない場合の表示

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,8 +13,8 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def default_url
-    '/icon/noimage.svg'
+  def default_url(*args)
+    '/icon/noimage1.svg'
   end
   
   process resize_to_fit: [400, 400]
